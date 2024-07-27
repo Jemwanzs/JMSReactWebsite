1bee0a3a8af1c53bcef3d259fe7aa3d936ae8de6
James Sammy <jamosammy@gmail.com>
2024-07-27 22:28:52 +0300

    Slider Updates

--- src/components/inc/Slider.js
+++ src/components/inc/Slider.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import sliderVideo1 from '../videos/video4.mp4'; // Replace with the path to your video files
 import sliderVideo2 from '../videos/video2.mp4';
 import sliderVideo3 from '../videos/video3.mp4';
+import './Slider.css'; // Import the CSS file
 
 function Slider() {
     return (
@@ -14,22 +15,32 @@ function Slider() {
             </div>
             <div className="carousel-inner">
                 <div className="carousel-item active">
-                    <video className="d-block w-100 slim-image" autoPlay loop muted>
-                        <source src={sliderVideo1} type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
+                    <div className="video-container">
+                        <video className="d-block w-100 slim-image" autoPlay loop muted>
+                            <source src={sliderVideo1} type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                        <div className="watermark">Led by James Sammy, a seasoned full-stack developer
+                            with over six years of experience, we specialize in tailored automation solutions for businesses.</div>
+                    </div>
                 </div>
                 <div className="carousel-item">
-                    <video className="d-block w-100 slim-image" autoPlay loop muted>
-                        <source src={sliderVideo2} type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
+                    <div className="video-container">
+                        <video className="d-block w-100 slim-image" autoPlay loop muted>
+                            <source src={sliderVideo2} type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                        <div className="watermark">Go to about us for more info.</div>
+                    </div>
                 </div>
                 <div className="carousel-item">
-                    <video className="d-block w-100 slim-image" autoPlay loop muted>
-                        <source src={sliderVideo3} type="video/mp4" />
-                        Your browser does not support the video tag.
-                    </video>
+                    <div className="video-container">
+                        <video className="d-block w-100 slim-image" autoPlay loop muted>
+                            <source src={sliderVideo3} type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                        <div className="watermark">Visit the Contact us and send a message!</div>
+                    </div>
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
