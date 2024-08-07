b379f294711747baa950a611141a48482d492eb2
James Sammy <jamosammy@gmail.com>
2024-08-07 15:20:24 +0300

    Purchase Page Updates

--- src/components/pages/Purchase.js
+++ src/components/pages/Purchase.js
@@ -12,7 +12,7 @@ import { Link } from 'react-router-dom';
 function Purchase() {
     const tvs = [
         { id: 1, src: TV1, title: "<Samsung QLED 55'' 4K>", price: "KES 120,000" },
-        { id: 2, src: TV2, title: "<LG OLED 55''-Inch''>", price: "KES 160,000" },
+        { id: 2, src: TV2, title: "<LG OLED 55''>", price: "KES 160,000" },
         { id: 3, src: TV3, title: "<Sony Bravia 65'' 4K", price: "KES 150,000" },
         { id: 4, src: TV4, title: "<TCL 50'' 4K UHD>", price: "KES 80,000" },
         { id: 5, src: TV5, title: "<Hisense 50'' ULED>", price: "KES 95,000" },
@@ -25,14 +25,16 @@ function Purchase() {
         <div className="row">
             {tvs.slice(startIndex, startIndex + 4).map(tv => (
                 <div key={tv.id} className="col-md-3 mb-4">
-                    <div className="card shadow">
-                        <img src={tv.src} className="w-100 border-bottom" alt={tv.title} />
-                        <div className="card-body">
-                            <h5>{tv.title}</h5>
-                            <p>{tv.price}</p>
-                            <Link to="/contact" className="btn btn-link">Order Now</Link>
+                    <Link to="/contact" className="card-link">
+                        <div className="card shadow">
+                            <img src={tv.src} className="w-100 border-bottom" alt={tv.title} />
+                            <div className="card-body">
+                                <h5>{tv.title}</h5>
+                                <p>{tv.price}</p>
+                                <button className="btn btn-link">Order Now</button>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 </div>
             ))}
         </div>
